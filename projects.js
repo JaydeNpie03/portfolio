958379c16f52b9762e3f7749472797578bf7d583
JaydeNpie03 <137616280+JaydeNpie03@users.noreply.github.com>
2023-12-01 21:57:32 -0600

    Update projects.js
    Updated project array with live references to their preview pictures

--- projects.js
+++ projects.js
@@ -138,7 +138,7 @@ document.addEventListener('DOMContentLoaded', () => {
         return {
           "1": {
             "title": "Tic-Tac-Toe",
-            "preview_pic": "images/ttt.png",
+            "preview_pic": "https://jaydenpie03.github.io/portfolio/images/ttt.png",
             "desc": "A simple game of tic-tac-toe in Java, utilizing Swing for UI.",
             "github": "https://github.com/JaydeNpie03/portfolio/tree/main/tictactoe",
             "type": "JAVA"
@@ -146,7 +146,7 @@ document.addEventListener('DOMContentLoaded', () => {
         
           "2": {
             "title": "Kazeni",
-            "preview_pic": "images/kazeniLogoSquare.png",
+            "preview_pic": "https://jaydenpie03.github.io/portfolio/images/kazeniLogoSquare.png",
             "desc": "A website that serves as a fun way to learn various Japanese Kanji. Uses KanjiAPI for kanji data.",
             "github": "https://github.com/JaydeNpie03/portfolio/tree/main/kazeni",
             "type": "HTML"
@@ -154,7 +154,7 @@ document.addEventListener('DOMContentLoaded', () => {
         
           "3": {
             "title": "Wiki",
-            "preview_pic": "images/wiki.png",
+            "preview_pic": "https://jaydenpie03.github.io/portfolio/images/wiki.png",
             "desc": "A simple wiki, allows user to search, add, and sort entries.",
             "github": "https://github.com/JaydeNpie03/portfolio/tree/main/wiki",
             "type": "HTML"
@@ -162,7 +162,7 @@ document.addEventListener('DOMContentLoaded', () => {
         
           "4": {
             "title": "Airline Booking",
-            "preview_pic": "images/airline.png",
+            "preview_pic": "https://jaydenpie03.github.io/portfolio/images/airline.png",
             "desc": "A simplified version of an airline booking site. Allows user to create a booking, search existing bookings, and see who's on which flights.",
             "github": "https://github.com/JaydeNpie03/portfolio/tree/main/airline",
             "type": "HTML"
