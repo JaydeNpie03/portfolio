090c751a708bb33d17a236c06e902bbd1598a2b3
JaydeNpie03 <137616280+JaydeNpie03@users.noreply.github.com>
2023-12-01 21:35:56 -0600

    Update projects_array.js
    Fixed Object declaration syntax

--- projects_array.js
+++ projects_array.js
@@ -1,29 +1,29 @@
 var project_array = {
-  1: {
+  "1": {
     "title": "Tic-Tac-Toe",
     "preview_pic": "images/ttt.png",
     "desc": "A simple game of tic-tac-toe in Java, utilizing Swing for UI.",
     "github": "https://github.com/JaydeNpie03/portfolio/tree/main/tictactoe",
     "type": "JAVA"
-    }
+    },
 
-    2: {
+  "2": {
     "title": "Kazeni",
     "preview_pic": "images/kazeniLogoSquare.png",
     "desc": "A website that serves as a fun way to learn various Japanese Kanji. Uses KanjiAPI for kanji data.",
     "github": "https://github.com/JaydeNpie03/portfolio/tree/main/kazeni",
     "type": "HTML"
-    }
+    },
 
-    3: {
+  "3": {
     "title": "Wiki",
     "preview_pic": "images/wiki.png",
     "desc": "A simple wiki, allows user to search, add, and sort entries.",
     "github": "https://github.com/JaydeNpie03/portfolio/tree/main/wiki",
     "type": "HTML"
-    }
+    },
 
-    4: {
+  "4": {
     "title": "Airline Booking",
     "preview_pic": "images/airline.png",
     "desc": "A simplified version of an airline booking site. Allows user to create a booking, search existing bookings, and see who's on which flights.",
@@ -31,3 +31,4 @@ var project_array = {
     "type": "HTML"
     }
 }
+
