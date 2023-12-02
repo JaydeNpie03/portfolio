c75de4c6678060024d618f417efb27abd3df8a44
JaydeNpie03 <137616280+JaydeNpie03@users.noreply.github.com>
2023-12-01 21:44:47 -0600

    Update projects.js
    Added project array object to replace 'fetch' function used in Django

--- projects.js
+++ projects.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+    var project_array = createArray();
     setupListeners();
     
     function setupListeners() {
@@ -83,58 +84,88 @@ document.addEventListener('DOMContentLoaded', () => {
                 hNav.classList.toggle('active', true)
                 getProjects('HTML');
             }
-            
         });
     }
-
+        
     function getProjects(type = 'all') {
-        fetch('/getprojects')
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById('row1').innerHTML = ''; //Reset Project Cards
-            for (p of data) {
-                if(type == 'all' || type == p['type']) {
-                    //Variables
-                    const colDiv = document.createElement('div');
-                    const cardDiv = document.createElement('div');
-                    const cardBody = document.createElement('div');
-                    const title = document.createElement('h3');
-                    const desc = document.createElement('p');
-                    const github = document.createElement('a');
-                    const previewPic = document.createElement('img');
+        document.getElementById('row1').innerHTML = ''; //Reset Project Cards
+        for (p of projectArray) {
+            if(type == 'all' || type == p['type']) {
+                //Variables
+                const colDiv = document.createElement('div');
+                const cardDiv = document.createElement('div');
+                const cardBody = document.createElement('div');
+                const title = document.createElement('h3');
+                const desc = document.createElement('p');
+                const github = document.createElement('a');
+                const previewPic = document.createElement('img');
 
-                    //Class Assignments
-                    colDiv.classList.add('col-lg-4', 'col-md-6', 'col-sm-12');
-                    cardDiv.classList.add('card', 'mb-4');
-                    previewPic.classList.add('card-img-top', 'rounded-5', 'px-2', 'pt-2');
-                    cardBody.classList.add('card-body');
-                    title.classList.add('card-title');
-                    desc.classList.add('card-text');
-                    github.classList.add('btn', 'btn-primary')
+                //Class Assignments
+                colDiv.classList.add('col-lg-4', 'col-md-6', 'col-sm-12');
+                cardDiv.classList.add('card', 'mb-4');
+                previewPic.classList.add('card-img-top', 'rounded-5', 'px-2', 'pt-2');
+                cardBody.classList.add('card-body');
+                title.classList.add('card-title');
+                desc.classList.add('card-text');
+                github.classList.add('btn', 'btn-primary')
 
-                    //Value Assignments
-                    colDiv.style.opacity = '0'
-                    previewPic.src = p['preview_pic'];
-                    title.innerHTML = p['title'];
-                    desc.innerHTML = p['desc'];
-                    github.innerHTML = 'View GitHub Repo';
-                    github.href = p['github'];
-                    github.target = '_blank';
-                    github.rel = 'noreferrer noopener';
+                //Value Assignments
+                colDiv.style.opacity = '0'
+                previewPic.src = p['preview_pic'];
+                title.innerHTML = p['title'];
+                desc.innerHTML = p['desc'];
+                github.innerHTML = 'View GitHub Repo';
+                github.href = p['github'];
+                github.target = '_blank';
+                github.rel = 'noreferrer noopener';
 
-                    //Build Card
-                    cardBody.appendChild(title);
-                    cardBody.appendChild(desc);
-                    cardBody.appendChild(github);
-                    cardDiv.appendChild(previewPic);
-                    cardDiv.appendChild(cardBody);
-                    colDiv.appendChild(cardDiv);
+                //Build Card
+                cardBody.appendChild(title);
+                cardBody.appendChild(desc);
+                cardBody.appendChild(github);
+                cardDiv.appendChild(previewPic);
+                cardDiv.appendChild(cardBody);
+                colDiv.appendChild(cardDiv);
 
-                    //Fade In Card
-                    document.getElementById('row1').appendChild(colDiv);
-                    colDiv.style.animation = 'fadeIn .5s ease-in 1 forwards running';
-                }
+                //Fade In Card
+                document.getElementById('row1').appendChild(colDiv);
+                colDiv.style.animation = 'fadeIn .5s ease-in 1 forwards running';
             }
-        });
+        })
     }
+
+     function createArray() {
+        return {
+          "1": {
+            "title": "Tic-Tac-Toe",
+            "preview_pic": "images/ttt.png",
+            "desc": "A simple game of tic-tac-toe in Java, utilizing Swing for UI.",
+            "github": "https://github.com/JaydeNpie03/portfolio/tree/main/tictactoe",
+            "type": "JAVA"
+            },
+        
+          "2": {
+            "title": "Kazeni",
+            "preview_pic": "images/kazeniLogoSquare.png",
+            "desc": "A website that serves as a fun way to learn various Japanese Kanji. Uses KanjiAPI for kanji data.",
+            "github": "https://github.com/JaydeNpie03/portfolio/tree/main/kazeni",
+            "type": "HTML"
+            },
+        
+          "3": {
+            "title": "Wiki",
+            "preview_pic": "images/wiki.png",
+            "desc": "A simple wiki, allows user to search, add, and sort entries.",
+            "github": "https://github.com/JaydeNpie03/portfolio/tree/main/wiki",
+            "type": "HTML"
+            },
+        
+          "4": {
+            "title": "Airline Booking",
+            "preview_pic": "images/airline.png",
+            "desc": "A simplified version of an airline booking site. Allows user to create a booking, search existing bookings, and see who's on which flights.",
+            "github": "https://github.com/JaydeNpie03/portfolio/tree/main/airline",
+            "type": "HTML"
+            }
+        }
 });
